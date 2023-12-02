0494daf63694c7f78de0ffc8cd2177c14bbc88d7
sriaman <amansrivastav.cse20@jecrc.ac.in>
2023-12-03 03:21:35 +0530

    Initial Commit

---
+++ src/main/java/com/ms/bankaccountdetails/BankAccountDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.ms.bankaccountdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountDetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountDetailsApplication.class, args);
+    }
+
+}
